6f081d3b8407d08abbfb7e08c5bcf732e83d0db7
Salih Çakmak <mehmetsalih.cakmak@vakifbank.com.tr>
2024-04-21 16:00:52 +0300

    First

---
+++ TrainingAnimation/TrainingAnimation/TrainingAnimationApp.swift
@@ -0,0 +1,20 @@
+//
+//  TrainingAnimationApp.swift
+//  TrainingAnimation
+//
+//  Created by Mehmet Salih ÇAKMAK (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAnimationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
